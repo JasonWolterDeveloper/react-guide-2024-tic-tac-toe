803e2c19c944ac98c7d817e2c5d0722657d9199d
JasonWolterDeveloper <143658631+JasonWolterDeveloper@users.noreply.github.com>
2024-03-22 20:27:32 -0500

    Can change player name and save new player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,22 +2,29 @@ import { useState } from "react"
 
 export default function Player({name, symbol}) {
     const [ isEditing, setIsEditing ] = useState(false);
+    const [ playerName, setPlayerName] = useState(name);
 
     const handleEditClick = () => {
-        setIsEditing(true);
-    }
+        setIsEditing((editing) => !editing);
+    };
 
-    let playerNameElement = <span className="player-name">{name}</span>
+    const handlePlayerNameChange = (event) => {
+        setPlayerName(event.target.value);
+    };
+
+
+    let playerNameElement = <span className="player-name">{playerName}</span>
+
 
     if (isEditing) {
-        playerNameElement = <input type="text" required />;
-    }
+        playerNameElement = <input onChange={handlePlayerNameChange} type="text" required value={playerName} />;
+    };
 
     return <li>
     <span className="player">
       {playerNameElement}
       <span className="player-symbol">{symbol}</span>
     </span>
-    <button onClick={handleEditClick}>Edit</button>
+    <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
   </li>
 }
